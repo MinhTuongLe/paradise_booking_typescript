c9da3bf5589cedcca5dbbe95dd7e83cf3acd4244
leminhtuong <leminhtuong09122002@gmail.com>
2024-03-29 23:55:24 +0700

    update next env config

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    GOOGLE_OAUTH_CLIENT_SECRET: "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",
+    GOOGLE_OAUTH_ENDPOINT: "http://localhost:3000",
+    GOOGLE_OAUTH_REDIRECT: "http://localhost:3000",
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
       ...[
