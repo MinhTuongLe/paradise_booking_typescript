f0cc75e85740ae826250f17d830a60a46315debf
LeMinhTuong <102809207+MinhTuongLe@users.noreply.github.com>
2024-07-14 14:43:59 +0700

    change bot id

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
     DEPLOYMENT_NAME: "paradisebookingchat",
     ENDPOINT: "https://bookingparadiseapp.openai.azure.com/",
     AZURE_KEY: "b50093f8b2fe4d4581346879dc211701",
-    BOT_ID: "app_32d1lnrs171m",
+    BOT_ID: "app_etaqwraq4nn0",
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
